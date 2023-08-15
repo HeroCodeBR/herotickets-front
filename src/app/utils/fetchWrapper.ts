e81fd751e40e743d15b77d8a0f71849bea4c3fd6
Alexia Kattah <alexiakattah@gmail.com>
2023-08-14 21:25:26 -0300

    change url default

--- src/app/utils/fetchWrapper.ts
+++ src/app/utils/fetchWrapper.ts
@@ -1,5 +1,8 @@
 export const fetchWrapper = async (url: string, options: any) => {
-  const response = await fetch(`http://localhost:3333${url}`, options);
+  const response = await fetch(
+    `https://rln471iuca.execute-api.us-east-1.amazonaws.com/dev${url}`,
+    options,
+  );
   const data = await response.json();
   return data;
 };
