82f99056be5fc89f086795c5f5ade9256282e585
Alexia Kattah <alexiakattah@gmail.com>
2023-09-01 12:09:08 -0300

    change url backend

--- src/app/utils/fetchWrapper.ts
+++ src/app/utils/fetchWrapper.ts
@@ -3,6 +3,7 @@ export const fetchWrapper = async (url: string, options: any) => {
     `https://pz14oglr35.execute-api.us-east-1.amazonaws.com/dev${url}`,
     options,
   );
+
   const data = await response.json();
   return data;
 };
